96edd05bbefeaf4ace1784e8dba74dfbd247a3a6
tatsuo yamaguchi <ta-yamaguchi@nile-inc.net>
2016-09-08 17:41:15 +0900

    ProjectSettingsを使うようにした

---
+++ Assets/SimpleTextureModifier/Scripts/Editor/TextureModifierSetting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace SimpleTextureModifier {
+public class TextureModifierSetting {
+	[PreferenceItem("TextureModifier")]
+    public static void ShowPreference() {
+		bool bValue;
+		bValue = SimpleTextureModifierSettings.Key;
+		bValue = EditorGUILayout.Toggle(TextureModifier.KEY, bValue);
+		bool fValue;
+		fValue = SimpleTextureModifierSettings.ForceSTMSetting;
+		fValue = EditorGUILayout.Toggle(TextureModifier.FORCESTMSETTING, fValue);
+		if (GUI.changed) {
+			SimpleTextureModifierSettings.Key = bValue;
+			SimpleTextureModifierSettings.ForceSTMSetting = fValue;
+			SimpleTextureModifierSettings.Save();
+		}
+    }
+}
+}
+	
